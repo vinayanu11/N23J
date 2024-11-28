ca8f829bae4ab44b5c3a81a92c91ab0c9167c8c8
DELL <DELL@Anu-Merin-Mathew>
2024-11-28 08:26:10 +0530

    28-11-2024: Adding variables.java

---
+++ javaPractice/src/main/java/basics/Variables.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		 int a;//declaration int b; a=5; //initialization b=10;
+		 
+		/*
+		 * int c=187; int d=187;//ctrl+shift+/
+		 */
+
+	}
+
+}
