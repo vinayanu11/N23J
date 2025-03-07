ccaecd1c0b4741b5fb1e4455920a472393c9b0fe
DELL <DELL@Anu-Merin-Mathew>
2025-03-07 13:56:44 +0530

    nn

---
+++ demo1/src/test/java/WebDriverManager.java
@@ -0,0 +1,31 @@
+
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverManager {
+	 private static WebDriver driver;
+
+	    private WebDriverManager() {
+	        // private constructor to prevent instantiation
+	    }
+
+	    public static WebDriver getDriver() {
+	        if (driver == null) {
+	            // Initialize the WebDriver here
+	            driver = new ChromeDriver();
+	            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	            driver.manage().window().maximize();
+	        }
+	        return driver;
+	    }
+
+	    public static void closeDriver() {
+	        if (driver != null) {
+	            driver.quit();
+	            driver = null;
+	        }
+	    }
+}
